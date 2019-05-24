becfbe9adb38522c4b671eb5ba4277f14147f3d9
Angus Griffith <16sn6uv@gmail.com>
2019-05-23 23:56:35 -0700

    tidy log statements

--- model.py
+++ model.py
@@ -7,18 +7,16 @@ class ModelBatchResults:
     '''
     The result of training the model on one batch.
     '''
-    def __init__(self, iteration, loss, precision, recall, accuracy):
-        self.iteration = iteration
+    def __init__(self, loss, precision, recall, accuracy):
         self.loss = loss
         self.precision = precision
         self.recall = recall
         self.accuracy = accuracy
 
     @staticmethod
-    def from_prediction(iteration, loss, prediction, actual_mines):
+    def from_prediction(loss, prediction, actual_mines):
         actual_mines = np.array(actual_mines)
-        result = ModelBatchResults(None, None, None, None, None)
-        result.iteration = iteration
+        result = ModelBatchResults(None, None, None, None)
         result.loss = loss
         predicted_mines = prediction > 0.5
         true_pos = np.sum(np.logical_and((predicted_mines == actual_mines), (predicted_mines == 1)))
@@ -27,15 +25,14 @@ class ModelBatchResults:
         result.accuracy = np.sum(predicted_mines == actual_mines) / prediction.size
         return result
 
-    def print(self):
-        print("Iteration %7i loss: %6.3f precision: %5.3f recall: %5.3f accuracy: %5.3f"
-          % (self.iteration, self.loss, self.precision, self.recall, self.accuracy))
+    def print(self, iteration, total_iterations):
+        print("%5.1f%% iteration: %7i loss: %6.3f precision: %5.3f recall: %5.3f accuracy: %5.3f"
+          % (100.0 * iteration / total_iterations, iteration, self.loss, self.precision, self.recall, self.accuracy))
 
     @staticmethod
     def combine(results):
         n = len(results)
-        result = ModelBatchResults(None, None, None, None, None)
-        result.iteration = max(r.iteration for r in results)
+        result = ModelBatchResults(None, None, None, None)
         result.loss = sum(r.loss for r in results) / n
         result.precision = sum(r.precision for r in results) / n
         result.recall = sum(r.recall for r in results) / n
@@ -97,7 +94,7 @@ class Model:
             print("Epoch %3i" % epoch)
             random.shuffle(examples)
             results = []
-            for idx in range(0, len(examples)-batch_size, batch_size):
+            for idx in range(0, len(examples), batch_size):
                 batch = examples[idx:idx+batch_size]
                 grids, ps = list(zip(*batch))
 
@@ -105,15 +102,18 @@ class Model:
                 results.append(result)
 
                 if idx % 50000 < batch_size:
-                    ModelBatchResults.combine(results).print()
+                    ModelBatchResults.combine(results).print(idx+batch_size, len(examples))
                     results = []
+            if results:
+                ModelBatchResults.combine(results).print(len(examples), len(examples))
+                results = []
 
     def train_batch(self, iteration, grids, ps):
         feed_dict = {self.x: grids, self.p_: ps}
         self.sess.run(self.train_step, feed_dict=feed_dict)
         loss = self.sess.run([self.loss], feed_dict=feed_dict)[0]
         pred = self.sess.run([self.p], feed_dict=feed_dict)[0]
-        return ModelBatchResults.from_prediction(iteration, loss, pred, ps)
+        return ModelBatchResults.from_prediction(loss, pred, ps)
 
     def predict(self, grid):
         """Evaluates the model to predict an output.
--- player_test.py
+++ player_test.py
@@ -8,9 +8,12 @@ from game import Game
 def test_player():
     random.seed(a=0)
     p = Player(3, 3, 1)
-    p.play(10, debug=True)
+    p.play(1, debug=True)
     assert(p.data)
-    p.train()
+    p.play(1000)
+    p.train(epochs=5, batch_size=16)
+    p.play(1000)
+    p.play(1, debug=True)
 
 
 if __name__ == '__main__':
