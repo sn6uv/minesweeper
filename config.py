4e47377928efd9a5c0c574f55174b5551b1e62a1
Angus Griffith <16sn6uv@gmail.com>
2019-06-05 21:58:54 -0700

    remove unused script

--- config.py
+++ config.py
@@ -3,10 +3,6 @@ HEIGHT = 9
 WIDTH = 9
 MINES = 10
 
-# Play params
-PLAY_BATCH_SIZE = 1000
-PLAY_ROUNDS = 100
-
 # Training params
 BATCH_SIZE = 1024
 LEARNING_RATE = 0.001
@@ -14,6 +10,3 @@ L2_REGULARISATION = 0.0001
 EPOCHS = 1
 PRINT_ITERATIONS=10000
 MODEL_PATH = "models/9_9_10/model.ckpt"
-
-# Data params
-DATA_DIR = "data/"
