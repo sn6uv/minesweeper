385d0e83bcc8f08ae1e08b776e5686f234afc209
Angus Griffith <16sn6uv@gmail.com>
2020-01-09 00:04:32 +1100

    cleanup learn_from_pretrained to use config.py

--- learn_from_pretrained.py
+++ learn_from_pretrained.py
@@ -1,5 +1,5 @@
 '''Script for training the model by playing games and trainging on them '''
-from config import HEIGHT, WIDTH, MINES
+from config import HEIGHT, WIDTH, MINES, BATCH_SIZE
 from player import Player
 from utils import play_train
 
@@ -9,7 +9,7 @@ def main():
     p.model.restore("pretrained")
     while True:
         p.play_debug()
-        play_train(p, 50000, 1, 32768)
+        play_train(p, 50000, 1, BATCH_SIZE)
         p.model.save("trained")
 
 
